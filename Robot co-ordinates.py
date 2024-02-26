70587585a5c0b748cf7b8869b853015e6837accf
Kathiravan.m <159284720+Kathir073@users.noreply.github.com>
2024-02-26 20:28:41 +0530

    Create Robot co-ordinates.py

---
+++ Robot co-ordinates.py
@@ -0,0 +1,24 @@
+"""
+The initial x and y co-ordinate values of a Robot are passed as the input.
+The rest of the input values are the directions in which the Robot moves along with the distance in that direction.
+The directions are denoted by N, E, S, W for North, East, South and West.
+The program should print the final x and y co-ordinates of the Robot
+INPUT:
+x-2 y3 W3 N1
+OUTPUT:
+x-5 y4
+"""
+l=list(input().strip().split())
+sumx,sumy=0,0
+for i in range(len(l)):
+    a=l[i]
+    ch,s=a[0],int(str(a[1:]))
+    if ch=='x' or ch=='E':
+        sumx=sumx+num
+    elif ch=='W':
+        sumx=sumx-num
+    elif ch=='N' or ch=='y':
+        sumy=sumy+num
+    else:
+        sumy=sumy-num
+print("x"+str(sumx)," y"+str(sumy))
